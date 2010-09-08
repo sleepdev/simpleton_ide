8191768a335bf0206b2a1281cce065cdf557f64e
andrew johnson <sleepdev@gmail.com>
2010-09-08 01:11:09 -0600

    Replaced classes with procedures. This way all window elements are directly accessible from the global namespace.

--- __main__.py
+++ __main__.py
@@ -3,36 +3,28 @@
 import gtk
 
 
-class MenuBar:
-   def __init__( self, parent ):
-      self.parent = parent
-      self.menubar = gtk.MenuBar()
-      self.parent.pack_start( self.menubar, expand=False )
 
-      menu_structure = [
-         ("File", [
-            ["New"], #[ord("N"),gtk.gdk.CONTROL_MASK]],
-            ["Open..."], #[ord("O"),gtk.gdk.CONTROL_MASK]],
-            [],
-            ["Save"],
-            ["Save As"],
-            ["Revert"],
-            [],
-            ["1. Most Recent"],
-            ["2. Second Most Recent"],
-            ["3. Third Most Recent"],
-            ["4. Fourth Most Recent"],
-            ["5. Fifth Most Recent"],
-            [],
-            ["Close"],
-            ["Quit", gtk.main_quit],
-         ]),
-      ]
+def MainWindow():
+   global window, window_vbox
+   window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+   window.set_title("TODO set correct title - Simpleton IDE")
+   window.set_default_size(1280,949)
+   window.connect('destroy', gtk.main_quit)
 
-      self.expand_menu( self.menubar, menu_structure )
-      
+   window_vbox = gtk.VBox()
+   window.add( window_vbox )
+   MenuBar()
+   Toolbar()
+   Notebook()
+   StatusBar()
+   window.show_all()
+    
+def MenuBar():
+   global menubar
+   menubar = gtk.MenuBar()
+   window_vbox.pack_start( menubar, expand=False )
 
-   def expand_menu( self, parent, structure ):
+   def expand_menu( parent, structure ):
       for entry in structure:
          if isinstance(entry,list):
             if len(entry)==0:
@@ -46,87 +38,91 @@ class MenuBar:
             sublabel = gtk.MenuItem(entry[0])
             submenu = gtk.Menu()
             sublabel.set_submenu(submenu)
-            self.expand_menu( submenu, entry[1] )
+            expand_menu( submenu, entry[1] )
             parent.append( sublabel )
 
-
-
-class Toolbar:
-   def __init__( self, parent ):
-      self.parent = parent
-      self.toolbar = gtk.Toolbar()
-
-      go_up = gtk.Button()
-      go_up.set_relief( gtk.RELIEF_NONE )
-      go_up.set_focus_on_click( False ); 
-      go_up.set_image( gtk.image_new_from_icon_name("stock_up", 5) )
-      self.toolbar.append_widget( go_up, "Open the parent context", None ) 
-
-      self.parent.pack_start( self.toolbar, expand=False )
+   menu_structure = [
+      ("File", [
+         ["New"], #[ord("N"),gtk.gdk.CONTROL_MASK]],
+         ["Open..."], #[ord("O"),gtk.gdk.CONTROL_MASK]],
+         [],
+         ["Save"],
+         ["Save As"],
+         ["Revert"],
+         [],
+         ["1. Most Recent"],
+         ["2. Second Most Recent"],
+         ["3. Third Most Recent"],
+         ["4. Fourth Most Recent"],
+         ["5. Fifth Most Recent"],
+         [],
+         ["Close"],
+         ["Quit", gtk.main_quit],
+      ]),
+   ]
+   expand_menu( menubar, menu_structure )
       
 
-class Notebook:
 
-   def __init__( self, parent ):
-      self.parent = parent
-      self.notebook = gtk.Notebook()   
 
-      self.append_page("content 1","page 1")
-      self.append_page("content 2","page 2")
+def Toolbar():
+   global toolbar
+   toolbar = gtk.Toolbar()
 
-      self.parent.pack_start( self.notebook, expand=True )
+   go_up = gtk.Button()
+   go_up.set_relief( gtk.RELIEF_NONE )
+   go_up.set_focus_on_click( False ); 
+   go_up.set_image( gtk.image_new_from_icon_name("stock_up", 5) )
+   toolbar.append_widget( go_up, "Open the parent context", None ) 
 
-   def append_page( self, content_text, label_text ):
-
-      #scrollable textbox
-      textbox = gtk.TextView()
-      textbox.get_buffer().set_text( content_text )
-      textframe = gtk.Frame()           
-      textframe.set_shadow_type( gtk.SHADOW_ETCHED_IN )
-      textframe.add( textbox )
-      sw = gtk.ScrolledWindow()
-      sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-      sw.add_with_viewport(textframe)
+   window_vbox.pack_start( toolbar, expand=False )
       
-      #notebook tab
-      tab_hbox = gtk.HBox( spacing=7 )
-      tab_hbox.add( gtk.image_new_from_icon_name("txt", 2) ) 
-      tab_hbox.add( gtk.Label(label_text) )
 
-      x_button = gtk.Button()
-      x_button.set_relief( gtk.RELIEF_NONE )
-      x_button.set_focus_on_click( False ); 
-      x_button.set_image( gtk.image_new_from_icon_name("cancel", 1) )
-      tab_hbox.add( x_button )      
-      tab_hbox.show_all()
-      self.notebook.append_page(sw,tab_hbox)
+def Notebook():
+   global notebook
+   notebook = gtk.Notebook()   
+   window_vbox.pack_start( notebook, expand=True )
+
+   append_page("content 1","page 1")
+   append_page("content 2","page 2")
+
+      
+def append_page( content_text, label_text ):
+
+   #scrollable textbox
+   textbox = gtk.TextView()
+   textbox.get_buffer().set_text( content_text )
+   textframe = gtk.Frame()           
+   textframe.set_shadow_type( gtk.SHADOW_ETCHED_IN )
+   textframe.add( textbox )
+   sw = gtk.ScrolledWindow()
+   sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+   sw.add_with_viewport(textframe)
+   
+   #notebook tab
+   tab_hbox = gtk.HBox( spacing=7 )
+   tab_hbox.add( gtk.image_new_from_icon_name("txt", 2) ) 
+   tab_hbox.add( gtk.Label(label_text) )
+
+   x_button = gtk.Button()
+   x_button.set_relief( gtk.RELIEF_NONE )
+   x_button.set_focus_on_click( False ); 
+   x_button.set_image( gtk.image_new_from_icon_name("cancel", 1) )
+   tab_hbox.add( x_button )      
+   tab_hbox.show_all()
+   notebook.append_page(sw,tab_hbox)
 
 
 
-class StatusBar:
-   def __init__( self, parent ):
-      self.parent = parent
-      self.statusbar = gtk.Statusbar()   
-      self.parent.pack_start( self.statusbar, expand=False )
+def StatusBar():
+   global statusbar
+   statusbar = gtk.Statusbar()   
+   window_vbox.pack_start( statusbar, expand=False )
    
 
 
-class MainWindow:
-   def __init__( self ):
-
-      self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-      self.window.set_title("TODO set correct title - Simpleton IDE")
-      self.window.set_default_size(1280,949)
-      self.window.connect('destroy', gtk.main_quit)
-
-      self.vbox = gtk.VBox()
-      self.window.add( self.vbox )
-      MenuBar( self.vbox )
-      Toolbar( self.vbox )
-      Notebook( self.vbox )
-      StatusBar( self.vbox )
-      self.window.show_all()
       
+
 
 
 
